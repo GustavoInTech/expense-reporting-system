a72788a4b9222b31e8713be2aff0c051446db5e1
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-09-07 12:19:14 -0300

    Adicionar anotações JPA na entidade Revenue

--- src/main/java/com/yourcompany/expense_management/entity/Revenue.java
+++ src/main/java/com/yourcompany/expense_management/entity/Revenue.java
@@ -3,17 +3,42 @@ package com.yourcompany.expense_management.entity;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "revenue")
 public class Revenue {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String description;
 
+    @Column(nullable = false)
     private BigDecimal amount;
 
+    @Column(nullable = false)
     private LocalDate date;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
+
+    /*
+     * @ManyToOne
+     * 
+     * @JoinColumn(name = "user_id")
+     * private User user;
+     */
 
     public Revenue(Long id, String description, BigDecimal amount, LocalDate date, Category category) {
         this.id = id;
