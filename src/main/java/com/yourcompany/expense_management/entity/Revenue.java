c44a9eb1262d08bbc00fa3616819a5c7d3cf8bcf
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-09-07 10:42:58 -0300

    implementando funcionalidade Revenue(receita)

---
+++ src/main/java/com/yourcompany/expense_management/entity/Revenue.java
@@ -0,0 +1,88 @@
+package com.yourcompany.expense_management.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Revenue {
+
+    private Long id;
+
+    private String description;
+
+    private BigDecimal amount;
+
+    private LocalDate date;
+
+    private Category category;
+
+    /**
+     * @return Long return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return BigDecimal return the amount
+     */
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return LocalDate return the date
+     */
+    public LocalDate getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    /**
+     * @return Category return the category
+     */
+    public Category getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+}
