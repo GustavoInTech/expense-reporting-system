376f0c675a9f0421a3ac6e4c9aa5e6e5fc0fd514
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-09-12 20:47:29 -0300

    Iniciando o DTO (Data Transfer Objects)

---
+++ src/main/java/com/yourcompany/expense_management/dto/BudgetDTO.java
@@ -0,0 +1,55 @@
+package com.yourcompany.expense_management.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class BudgetDTO {
+
+    private Long id;
+    private String name;
+    private BigDecimal amount;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
