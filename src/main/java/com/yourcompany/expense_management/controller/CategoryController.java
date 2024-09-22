df9bdfd299e280ca4a318249b8eefa3dc6c6af34
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-09-21 22:51:41 -0300

    adicionando contrller de category

--- src/main/java/com/yourcompany/expense_management/controller/CategoryController.java
+++ src/main/java/com/yourcompany/expense_management/controller/CategoryController.java
@@ -1,6 +1,6 @@
 package com.yourcompany.expense_management.controller;
 
-import com.yourcompany.expense_management.entity.Category;
+import com.yourcompany.expense_management.dto.CategoryDTO;
 import com.yourcompany.expense_management.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/categories")
@@ -18,32 +17,39 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public ResponseEntity<List<CategoryDTO>> getAllCategories() {
+        List<CategoryDTO> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id) {
         return categoryService.getCategoryById(id)
                 .map(category -> ResponseEntity.ok().body(category))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Category createCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO createdCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.ok(createdCategory);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
-        categoryService.deleteCategory(id);
-        return ResponseEntity.noContent().build();
+        boolean deleted = categoryService.deleteCategory(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Optional<Object>> updateCategory(@PathVariable Long id,
-            @RequestBody Category newCategoryData) {
-        Optional<Object> updatedCategory = categoryService.updateCategory(id, newCategoryData);
-        return ResponseEntity.ok(updatedCategory);
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id,
+            @RequestBody CategoryDTO newCategoryData) {
+        return categoryService.updateCategory(id, newCategoryData)
+                .map(updatedCategory -> ResponseEntity.ok(updatedCategory))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
